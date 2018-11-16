8132a8a473851b7a8f42b46f37c6f15cfab8ba60
aj-bhagat <ajaybhagatm620@gmail.com>
2018-11-17 00:59:45 +0530

    AngularApp

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,19 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'men', component: MenComponent },
+  { path: 'women',      component: WomenComponent },
+  { path: 'home',      component: HomeComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MenComponent,
+    WomenComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
