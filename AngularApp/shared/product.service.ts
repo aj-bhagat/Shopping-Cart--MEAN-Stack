2be4844b77cead6ab28c7acecd5e90115655f83d
aj-bhagat <ajaybhagatm620@gmail.com>
2018-11-27 01:01:21 +0530

    AngularApp

---
+++ AngularApp/shared/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import{Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {Product} from './product'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  article: Product[];
+  readonly baseURL: string="http://localhost:3000/articles";
+  constructor(private http: HttpClient) { }
+  getArticle(){
+    return this.http.get(this.baseURL);
+  }
+}
---
+++ AngularApp/shared/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    _id: string;
+    imagepath: String;
+    brand: String;
+    category: String;
+    price: String;
+    details: String
+}
